71cda248b6cebaf3bf0c23c7c2efa7152a537403
pedro rebouças veloso <pedroreboucasveloso@gmail.com>
2025-09-25 14:12:18 -0300

    Update filmes_git.py

--- filmes_git.py
+++ filmes_git.py
@@ -201,6 +201,7 @@ def carregar_series(imdb_basics_path: str, imdb_ratings_path: str, min_votes: in
     basics = pd.read_csv(imdb_basics_path, sep="\t", low_memory=False, na_values="\\N")
     ratings = pd.read_csv(imdb_ratings_path, sep="\t", low_memory=False, na_values="\\N")
     
+    # ADICIONADO: O filtro de séries, agora que a coluna existe
     if "titleType" in basics.columns:
         basics = basics[basics["titleType"].isin(["tvSeries", "tvMiniSeries"])].copy()
     else:
