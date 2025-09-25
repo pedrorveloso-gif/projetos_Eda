54b647e9a31f35d7b1a023273d5a0fa7864fbb8d
pedro rebouças veloso <pedroreboucasveloso@gmail.com>
2025-09-25 15:21:34 -0300

    Update final_git.py

--- final_git.py
+++ final_git.py
@@ -1,5 +1,4 @@
-# arquivo: recomendador_git.py
-# Para rodar: streamlit run recomendador_git.py
+
 
 import streamlit as st
 import pandas as pd
@@ -198,8 +197,8 @@ def carregar_filmes(filmes_path: str) -> List[Filme]:
 
 @st.cache_data(show_spinner=False)
 def carregar_series(imdb_basics_path: str, imdb_ratings_path: str, min_votes: int = 500) -> List[Serie]:
-    basics = pd.read_csv(imdb_basics_path, sep="\t", low_memory=False, na_values="\\N", engine='python')
-    ratings = pd.read_csv(imdb_ratings_path, sep="\t", low_memory=False, na_values="\\N", engine='python')
+    basics = pd.read_csv(imdb_basics_path, sep="\t", low_memory=False, na_values="\\N")
+    ratings = pd.read_csv(imdb_ratings_path, sep="\t", low_memory=False, na_values="\\N")
     
     if "titleType" in basics.columns:
         basics = basics[basics["titleType"].isin(["tvSeries", "tvMiniSeries"])].copy()
@@ -394,3 +393,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
