a19d0ad3fe060bbed8ee37b5b58de4613a9b13a4
pedro rebouças veloso <pedroreboucasveloso@gmail.com>
2025-09-24 21:45:23 -0300

    Update final_filmes.py

--- final_filmes.py
+++ final_filmes.py
@@ -241,9 +241,9 @@ def construir_grafo(filmes: List[Filme], series: List[Serie]) -> GenreGraph:
 # =========================
 # 6) CAMINHOS DOS DADOS
 # =========================
-ZIP_FILMES = r"C:/Users/pedro/OneDrive/Documentos/projeto eda/archive.zip"  # contém movies_metadata.csv
-IMDB_BASICS = r"C:/Users/pedro/OneDrive/Documentos/projeto eda/title.basics.tsv.gz"
-IMDB_RATINGS = r"C:/Users/pedro/OneDrive/Documentos/projeto eda/title.ratings.tsv.gz"
+ZIP_FILMES = "archive_min.zip"
+IMDB_BASICS = "title.basics.min.tsv.gz"
+IMDB_RATINGS = "title.ratings.min.tsv.gz"
 
 # =========================
 # 7) UI
@@ -374,3 +374,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
