3d04fc4b4a930e04589605ade3f0eb80754e7355
pedro rebouças veloso <pedroreboucasveloso@gmail.com>
2025-09-25 11:07:23 -0300

    Update filmes_git.py

--- filmes_git.py
+++ filmes_git.py
@@ -197,7 +197,7 @@ def carregar_filmes(filmes_path: str) -> List[Filme]:
     return filmes
 
 @st.cache_data(show_spinner=False)
-def carregar_series(imdb_basics_path: str, imdb_ratings_path: str, min_votes: int = 500) -> List[Serie]:
+def carregar_series(imdb_basics_path: str, imdb_ratings_path: str, min_votes: int = 0) -> List[Serie]:
     basics = pd.read_csv(imdb_basics_path, sep="\t", low_memory=False, na_values="\\N")
     ratings = pd.read_csv(imdb_ratings_path, sep="\t", low_memory=False, na_values="\\N")
     
@@ -389,6 +389,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
